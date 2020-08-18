ce4e13e219f784d6b442e9318ecf4c1a6c8c222e
AhmedIbrahimHamed <aih1175@gmail.com>
2020-08-18 21:10:29 +0200

    Added xml output format to api

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -6,13 +6,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API {
     public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o => {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });
+            //.AddJsonOptions(o => {
+            //     if (o.SerializerSettings.ContractResolver != null) {
+            //         var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+            //         castedResolver.NamingStrategy = null;
+            //     }
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
