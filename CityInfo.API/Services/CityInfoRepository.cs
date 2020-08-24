3b25e263eb5805704afdcd031b957a53899e31ee
AhmedIbrahimHamed <aih1175@gmail.com>
2020-08-24 04:45:16 +0200

    Added Repositry pattern to project

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Contexts;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services {
+    public class CityInfoRepository : ICityInfoRepository {
+
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context) {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IEnumerable<City> GetCities() {
+            return _context.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointsOfInterest) {
+            if (includePointsOfInterest) {
+                return _context.Cities.Include(c => c.PointOfInterests).Where(c => c.Id == cityId).FirstOrDefault();
+            } else {
+                return _context.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+
+            }
+        }
+
+        public PointOfInterest GetPointOfInterest(int cityId, int PointOfInterestId) {
+            return _context.PointOfInterests.Where(p => p.CityId == cityId && p.Id == PointOfInterestId).FirstOrDefault();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId) {
+            return _context.PointOfInterests.Where(p => p.CityId == cityId ).ToList();
+
+        }
+    }
+}
